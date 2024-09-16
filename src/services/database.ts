f24cd2acd9af1801432dc6f9f6b32feb060d49de
supr1yo <supriyo.developer@gmail.com>
2024-09-16 10:03:32 +0530

    add database connection

---
+++ src/services/database.ts
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants";
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+if (!MONGODB_URI) {
+  throw new Error("Please define the MONGO_URI in .env file.");
+}
+ 
+const database = async () =>{
+  try {
+    await mongoose.connect(`${MONGODB_URI}/${DB_NAME}`, { bufferCommands: false }).then(() => {
+        console.log("Connected to MongoDB");
+      });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export default database;
