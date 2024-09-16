2d47215490155430a290331ec344b30ab32d5d2f
supr1yo <supriyo.developer@gmail.com>
2024-09-16 09:28:27 +0530

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,29 @@
-console.log("Hello World!");
+import compression from "compression";
+
+const express = require('express');
+const { createServer } = require('http');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const multer = require('multer');
+
+const PORT = process.env.PORT || 8080;
+const app = express();
+
+
+// Use middlewares
+app.use(cors({
+    credentials: true
+}));
+app.use(cookieParser());
+app.use(compression());
+app.use(bodyParser.json());
+
+const server = createServer(app);
+
+
+
+
+server.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+})
