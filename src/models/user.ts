2120a17ccd6349056e3bf835678f806cde270a55
supr1yo <supriyo.developer@gmail.com>
2024-09-16 14:08:58 +0530

    add user model

---
+++ src/models/user.ts
@@ -0,0 +1,26 @@
+import { Schema, model } from 'mongoose';
+import bcrypt from "bcrypt";
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+    },
+    password: {
+        type: String,
+        required: [true, 'Password is required'],
+        select: false,
+    }
+}, {
+    timestamps: true
+});
+
+export const User = model('User', userSchema);
