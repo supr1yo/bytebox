51c4082a8cb876ea16338628d6a8af3f996188e0
supr1yo <supriyo.developer@gmail.com>
2024-09-16 10:12:53 +0530

    remove dotenv

--- src/services/database.ts
+++ src/services/database.ts
@@ -1,9 +1,6 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants";
 
-const dotenv = require('dotenv');
-dotenv.config();
-
 const MONGODB_URI = process.env.MONGODB_URI;
 
 if (!MONGODB_URI) {
