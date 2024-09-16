cb4aef50dd5adbc31a28c7856efceb9803f7812e
supr1yo <supriyo.developer@gmail.com>
2024-09-16 14:09:17 +0530

    Add db queries function

--- src/services/queries.ts
+++ src/services/queries.ts
@@ -1,1 +1,16 @@
-// export const getUsers = () => 
+import { User } from '../models/user';
+
+
+export const getUsers = () => User.find();
+
+export const getUserByEmail = (email: string) => User.findOne({ email });
+
+export const getUserById = (id: string) => User.findById(id);
+
+export const createUser = (username: string, email: string, password: string) => User.create({
+    username, email, password
+});
+
+export const deleteUserById = (id: string) => User.findByIdAndDelete(id);
+
+
