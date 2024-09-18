53ead065d35fcfb2a633f19544a1c0f6f89d1bb6
supr1yo <supriyo.developer@gmail.com>
2024-09-18 19:28:56 +0530

    Add route to home

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home/Home';
 import LoginPage from './pages/Login';
 import SignupPage from './pages/Signup';
+
 export default function App() {
   return (
     <Router>
       <Routes>
+        <Route path='/' element={<Home />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
       </Routes>
