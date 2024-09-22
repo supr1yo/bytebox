565ae0e01bdbe611d36dae5df21593b2f85106f4
supr1yo <supriyo.developer@gmail.com>
2024-09-22 14:51:58 +0530

    feat: gridfs

--- server/src/database/index.ts
+++ server/src/database/index.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import Grid from 'gridfs-stream';
 import { DB_NAME } from "../utils/constants";
 
 const MONGODB_URI = process.env.MONGODB_URI;
@@ -9,9 +10,22 @@ if (!MONGODB_URI) {
  
 const database = async () =>{
   try {
-    await mongoose.connect(`${MONGODB_URI}/${DB_NAME}?retryWrites=true&w=majority`, { bufferCommands: false }).then(() => {
-        console.log("Connected to MongoDB");
-      });
+    // await mongoose.connect(`${MONGODB_URI}/${DB_NAME}?retryWrites=true&w=majority`, { bufferCommands: false }).then(() => {
+    //     console.log("Connected to MongoDB");
+    //   });
+     
+
+    // Create a connection to MongoDB
+    const conn = mongoose.createConnection(`${MONGODB_URI}/${DB_NAME}?retryWrites=true&w=majority`);
+
+    // Initialize GridFS stream
+    let gfs;
+
+    conn.once('open', () => {
+      gfs = Grid(conn.db, mongoose.mongo);
+      gfs.collection('uploads');
+      console.log('MongoDB connected.');
+    });
   } catch (error) {
     console.log(error);
   }
