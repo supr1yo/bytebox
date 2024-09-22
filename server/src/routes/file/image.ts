88d41d1be3e3d74b117ba967867c2d9670f94083
supr1yo <supriyo.developer@gmail.com>
2024-09-22 14:47:21 +0530

    removed

--- server/src/routes/file/image.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { uploadVideo } from '../../controllers/file.controller';
-import { upload } from '../../middlewares/multer';
-const router = Router();
-
-router.post('/upload/image', upload.single('image'), uploadVideo);
-
-export default router;
