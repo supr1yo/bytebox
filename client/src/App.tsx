64e776f883590eaa984d4f16d74a9be7f786eea2
supr1yo <supriyo.developer@gmail.com>
2024-09-22 16:04:00 +0530

    Update file path

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home';
-import LoginPage from './pages/Login';
+import LoginPage from './pages/Login/Login';
 import SignupPage from './pages/Signup';
 
 export default function App() {
