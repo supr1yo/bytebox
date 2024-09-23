444ae4ef21fc03d6c25a12a3b1e7cfd4ea529a09
supr1yo <supriyo.developer@gmail.com>
2024-09-23 20:04:49 +0530

    Import pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,17 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home/Home';
+import HomePage from './pages/Home/Home';
 import LoginPage from './pages/Login/Login';
-import SignupPage from './pages/Signup';
+import SignupPage from './pages/Signup/Signup';
+import DashboardPage from './pages/Dashboard/Dashboard';
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path='/' element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
+        <Route path='/dashboard' element={<DashboardPage />} />
       </Routes>
     </Router>
   );
