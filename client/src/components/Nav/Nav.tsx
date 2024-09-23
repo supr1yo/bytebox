ca5bf8688a0be1995ed8bed13fab54973592a209
supr1yo <supriyo.developer@gmail.com>
2024-09-23 20:05:11 +0530

    Update Navbar

--- client/src/components/Nav/Nav.tsx
+++ client/src/components/Nav/Nav.tsx
@@ -3,15 +3,14 @@ import styles from './Nav.module.css';
 export default function Nav() {
     return (
         <>
-        <div className={styles.nav}>
-            <ul>
-                <li>Home</li>
-                <li>Sign up</li>
-                <li>Log in</li>
-                <li>About</li>
-                <li>Contact us</li>
-            </ul>
-        </div>
+            <div className={styles.nav}>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="/dashboard">Dashboard</a></li>
+                    <li><a href="/about">About</a></li>
+                    <li><a href="/Contact">Contact</a></li>
+                </ul>
+            </div>
         </>
-    )
+    );
 }
