f776048dca99a7be8f857f80a643d46a0283b1a5
supr1yo <supriyo.developer@gmail.com>
2024-09-25 13:06:29 +0530

    Add 404 page.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,8 @@ import HomePage from './pages/Home/Home';
 import LoginPage from './pages/Login/Login';
 import SignupPage from './pages/Signup/Signup';
 import DashboardPage from './pages/Dashboard/Dashboard';
+import NotFound from './pages/404/404';
+
 
 export default function App() {
   return (
@@ -12,6 +14,7 @@ export default function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
         <Route path='/dashboard' element={<DashboardPage />} />
+        <Route path='*' element={<NotFound/>} />
       </Routes>
     </Router>
   );
